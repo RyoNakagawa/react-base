cf2b0fe575eb2a3e538cf9f6f80526888682347b
中川諒 <nakagawa@nakagawaryou-no-MacBook-Pro.local>
2020-07-13 22:10:46 +0900

    Initial commit

---
+++ src/configureStore.ts
@@ -0,0 +1,22 @@
+import { createBrowserHistory } from 'history'
+import { applyMiddleware, compose, createStore } from 'redux'
+import { routerMiddleware } from 'connected-react-router'
+import createRootReducer from './reducers'
+
+export const history = createBrowserHistory()
+const storeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export default function configureStore(preloadedState?: any) {
+  const store = createStore(
+    createRootReducer(history), // root reducer with router state
+    preloadedState,
+    storeEnhancers(
+      applyMiddleware(
+        routerMiddleware(history), // for dispatching history actions
+        // ... other middlewares ...
+      ),
+    ),
+  )
+
+  return store
+}
