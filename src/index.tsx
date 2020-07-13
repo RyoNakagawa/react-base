be14fdd8b00865cf2e51964f4e20fa10532c86c4
中川諒 <nakagawa@nakagawaryou-no-MacBook-Pro.local>
2020-07-13 22:28:25 +0900

    ディレクトリ構造を修正

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { Route, Switch } from 'react-router';
 import { ConnectedRouter } from 'connected-react-router'
 import './index.css';
 import App from './App';
@@ -14,9 +13,7 @@ ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <ConnectedRouter history={history}>
-        <Switch>
-          <Route exact path="/" component={App} />
-        </Switch>
+        <App />
       </ConnectedRouter>
     </Provider>
   </React.StrictMode>,
